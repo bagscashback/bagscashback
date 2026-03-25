1f93a6b1aca6bd6117df80e58e885f8612afa875
bagscashback <renjettaskon42@gmail.com>
2026-03-26 06:09:44 +0700

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * Bags.fm Ecosystem Constants
+ * Updated: March 2026
+ */
+
+export const BAGS_PROGRAM_ID = 'Bags777...'; // Official Bags Solana Program ID
+export const BAGS_FEE_ACCOUNT = 'BagsFee...'; // Global Treasury for Fee Sharing
+export const BAGS_APP_STORE_VERSION = 'v2.0.4';
+
+export const EXPLORER_URL = (sig: string) => `https://solscan.io/tx/${sig}`;
